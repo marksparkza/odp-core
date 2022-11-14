3db5b32b5c7814aac44bd500cb380853882141e3
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2022-11-14 15:59:07 +0200

    Assign a staff or default role on signup

--- odp/const/__init__.py
+++ odp/const/__init__.py
@@ -12,6 +12,8 @@ DOI_REGEX = r'^10\.\d{4,}(\.\d+)*/[-._;()/:a-zA-Z0-9]+$'
 # the suffix part of the DOI regex suffices for secondary IDs
 SID_REGEX = r'^[-._;()/:a-zA-Z0-9]+$'
 
+SAEON_EMAIL_DOMAINS = ['saeon.ac.za', 'saeon.nrf.ac.za']
+
 
 class ODPScope(str, Enum):
     CATALOG_READ = 'odp.catalog:read'
