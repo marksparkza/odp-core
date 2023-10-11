9aa860b2a9e638fa5bf4ea30fb11a0bc2631ebce
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2023-10-11 11:42:25 +0200

    Resolve MIMS DOIs to the MIMS catalogue

--- odp/config/datacite.py
+++ odp/config/datacite.py
@@ -11,8 +11,5 @@ class DataciteConfig(BaseConfig):
     # Note: DataCite's test API should be used in non-production environments
     API_URL: AnyHttpUrl
 
-    # base URL for redirects from doi.org
-    REDIRECT_URL: AnyHttpUrl
-
     USERNAME: str  # DataCite account username
     PASSWORD: str  # DataCite account password
