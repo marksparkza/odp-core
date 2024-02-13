00cd88c0423350473eebc6dc55a3ee5a9d5fec14
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2024-02-13 09:23:39 +0200

    Move auth response structures from server to core

--- odp/api/models/auth.py
+++ odp/api/models/auth.py
@@ -1,12 +1,32 @@
+from dataclasses import dataclass
 from typing import Literal, Optional
 
 from pydantic import BaseModel
+
+Permissions = dict[str, Literal['*'] | list[str]]
+"""The effective set of permissions for a user or a client. A dictionary of
+scope ids (OAuth2 scope identifiers), where the value for each id is either:
+
+- '*' if the scope is applicable across all relevant platform entities; or
+- a set of collection ids to which the scope's usage is limited (implemented
+  as a list for JSON serialization)
+"""
+
+
+@dataclass
+class UserInfo:
+    sub: str
+    email: str
+    email_verified: bool
+    name: Optional[str]
+    picture: Optional[str]
+    roles: list[str]
 
 
 class AccessTokenModel(BaseModel):
     client_id: str
     user_id: Optional[str]
-    permissions: dict[str, Literal['*'] | list[str]]
+    permissions: Permissions
 
 
 class ScopeModel(BaseModel):
