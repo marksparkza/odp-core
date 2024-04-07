2128ae76e4d876770f53dc2c5fe236b5b3075b06
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2024-04-07 18:53:24 +0200

    Implement package creation :gift:

--- odp/api/models/package.py
+++ odp/api/models/package.py
@@ -1,21 +1,35 @@
 from typing import Any, Optional
 
-from odp.api.models.resource import ResourceModel, ResourceModelIn
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+
+from odp.const import ODPMetadataSchema
 
 
 class PackageModel(BaseModel):
     id: str
     provider_id: str
     provider_key: str
-    record_id: Optional[str]
-    metadata: Optional[dict[str, Any]]
+    schema_id: str
+    metadata: dict[str, Any]
+    validity: dict[str, Any]
+    notes: Optional[str]
     timestamp: str
-    resources: list[ResourceModel]
+    resource_count: int
+    record_id: Optional[str]
+    record_doi: Optional[str]
+    record_sid: Optional[str]
 
 
 class PackageModelIn(BaseModel):
     provider_id: str
-    metadata: Optional[dict[str, Any]]
-    resource_ids: list[str]  # existing
-    resources: list[ResourceModelIn]  # new
+    schema_id: str
+    metadata: dict[str, Any]
+    notes: Optional[str]
+    resource_ids: list[str]
+
+    @validator('schema_id')
+    def validate_schema_id(cls, schema_id):
+        if schema_id not in (ODPMetadataSchema.SAEON_DATACITE4, ODPMetadataSchema.SAEON_ISO19115):
+            raise ValueError("SAEON metadata schema required")
+
+        return schema_id
