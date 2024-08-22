bee8a7eceb47ea6f9577cb6b23869d0af31f4289
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2024-08-22 19:14:19 +0200

    Add scope and adapter to archive config API output model

--- odp/api/models/archive.py
+++ odp/api/models/archive.py
@@ -2,10 +2,14 @@ from typing import Optional
 
 from pydantic import BaseModel
 
+from odp.const.db import ArchiveAdapter
+
 
 class ArchiveModel(BaseModel):
     id: str
     url: str
+    adapter: ArchiveAdapter
+    scope_id: str
     resource_count: int
 
 
