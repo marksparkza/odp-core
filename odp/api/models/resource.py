59848adbdc72d90cccd00f3c0da7d03ece0b4926
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2024-09-20 12:20:42 +0200

    Update resource API with title and hashing changes

--- odp/api/models/resource.py
+++ odp/api/models/resource.py
@@ -1,16 +1,19 @@
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
+
+from odp.const.db import HashAlgorithm
 
 
 class ResourceModel(BaseModel):
     id: str
-    title: str
+    title: Optional[str]
     description: Optional[str]
     filename: Optional[str]
     mimetype: Optional[str]
     size: Optional[int]
-    md5: Optional[str]
+    hash: Optional[str]
+    hash_algorithm: Optional[HashAlgorithm]
     timestamp: str
     provider_id: str
     provider_key: str
@@ -18,12 +21,30 @@ class ResourceModel(BaseModel):
 
 
 class ResourceModelIn(BaseModel):
-    title: str
+    title: Optional[str]
     description: Optional[str]
     filename: Optional[str]
     mimetype: Optional[str]
     size: Optional[int]
-    md5: Optional[str]
+    hash_algorithm: Optional[HashAlgorithm]
+    hash: Optional[str]
     provider_id: str
     archive_id: str
     archive_path: str = Field(..., regex=r'^\S*$', title='Resource path relative to archive URL')
+
+    @validator('filename', always=True)
+    def validate_filename(cls, filename, values):
+        if not filename and not values.get('title'):
+            raise ValueError('title and/or filename must be given')
+
+        return filename
+
+    @validator('hash')
+    def validate_hash(cls, hash, values):
+        try:
+            if hash is not None and not values['hash_algorithm']:
+                raise ValueError('hash_algorithm must be supplied with hash')
+        except KeyError:
+            pass  # ignore: hash_algorithm already failed validation
+
+        return hash
