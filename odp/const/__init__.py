76067d8e7564f84cd2f2a1068ed2b3cf610503af
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2025-03-17 10:38:55 +0200

    Improve UX for ORCID and ROR entry

--- odp/const/__init__.py
+++ odp/const/__init__.py
@@ -15,10 +15,12 @@ DOI_REGEX = r'^10\.\d{4,}(\.\d+)*/[-._;()/:a-zA-Z0-9]+$'
 SID_REGEX = r'^[-._;()/:a-zA-Z0-9]+$'
 
 # an ROR ID, from https://ror.readme.io/docs/identifier
-ROR_REGEX = r'^https://ror\.org/0[a-hj-km-np-tv-z|0-9]{6}[0-9]{2}$'
+ROR_PATH = r'0[a-hj-km-np-tv-z|0-9]{6}[0-9]{2}'
+ROR_REGEX = r'^https://ror\.org/%s$' % ROR_PATH
 
 # https://support.orcid.org/hc/en-us/articles/360006897674-Structure-of-the-ORCID-Identifier
-ORCID_REGEX = r'^https://orcid.org/(\d{4}-){3}\d{3}(\d|X)$'
+ORCID_PATH = r'(\d{4}-){3}\d{3}(\d|X)'
+ORCID_REGEX = r'^https://orcid.org/%s$' % ORCID_PATH
 
 SAEON_EMAIL_DOMAINS = ['saeon.ac.za', 'saeon.nrf.ac.za']
 
