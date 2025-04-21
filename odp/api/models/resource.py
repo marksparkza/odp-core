c1b4ab64f8f3f84eaaf691f955bdecda7aa580cb
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2025-04-21 12:40:35 +0200

    Replace existing objects
    It's a PUT. This is expected.
    
    Also, combine folder and filename into path across the board. This simplifies path integrity checks and validation, and parts can in any case be easily inferred by the client. If in future we need to distinguish file and folder resource types, this can be done explicitly with an enum on resource, instead of implicitly based on folder/filename values.

--- odp/api/models/resource.py
+++ odp/api/models/resource.py
@@ -2,21 +2,20 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 
-from odp.const.db import HashAlgorithm
+from odp.const.db import HashAlgorithm, ResourceStatus
 
 
 class ResourceModel(BaseModel):
     id: str
     title: Optional[str]
     description: Optional[str]
-    folder: str
-    filename: str
+    path: str
     mimetype: Optional[str]
     size: Optional[int]
     hash: Optional[str]
     hash_algorithm: Optional[HashAlgorithm]
+    status: ResourceStatus
     timestamp: str
     package_id: str
     package_key: str
-    package_path: str
     archive_paths: dict[str, str] = Field(..., title='Mapping of archive id to resource path relative to archive url(s)')
